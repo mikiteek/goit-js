2e5a3086b0b31c4c6d706fd5a6217bb06c903e70
mikiteek <mikiteek@gmail.com>
2020-07-12 13:37:43 +0300

    mod-08 -> move directory in new branch

--- hw-06/js/task-04.js
+++
@@ -1,9 +0,0 @@
-// task-04
-// Получить массив только неактивных пользователей (поле isActive).
-
-import users from "./users.js";
-
-const getInactiveUsers = users => users.filter(user => user.isActive === false);
-
-console.log(getInactiveUsers(users));
-// [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
