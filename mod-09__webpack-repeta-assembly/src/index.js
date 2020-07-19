07a0f79651c3cb0045a9c430b64903731bc8ee90
mikiteek <mikiteek@gmail.com>
2020-07-19 14:06:48 +0300

    mod-09 -> webpack-repeta: naming export

--- mod-09__webpack-repeta-assembly/src/index.js
+++ mod-09__webpack-repeta-assembly/src/index.js
@@ -1,6 +1,10 @@
-import './styles.scss';
-
 import createBox from "./js/create-box";
+import {add} from "./js/calc";
+import * as calc from "./js/calc";
+import './styles.scss';
 
 document.body.appendChild(createBox("Webpack is amazing"));
 
+console.log(add);
+console.log(calc);
+console.log(calc.add);
