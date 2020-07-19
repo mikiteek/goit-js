d56e00e1e5aafb01ac917b71f025df5b67be9bae
mikiteek <mikiteek@gmail.com>
2020-07-19 11:23:31 +0300

    mod-09 -> webpack: webpack.config.js, setting babel, css-loader, style-loader

---
+++ mod-09__webpack/src/index.js
@@ -0,0 +1,7 @@
+import value from "./js/a";
+import "./css/styles.css";
+
+const add = (a, b) => a + b;
+add(2,3);
+
+console.log(value);
---
+++ mod-09__webpack/webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: "babel-loader"
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", MiniCssExtractPlugin.loader, "css-loader"]
+      }
+    ]
+  },
+  plugins: [new MiniCssExtractPlugin()]
+};
