3ffa8cd3054ad8d0dfac8c3efc680a231619359b
mikiteek <mikiteek@gmail.com>
2020-07-20 10:12:37 +0300

    mod-10 -> libraries: basicLightbox modal window

--- mod-10__libraries/src/index.js
+++ mod-10__libraries/src/index.js
@@ -1,4 +1,25 @@
-import * as basicLightbox from 'basiclightbox'
-import './sass/styles.scss';
+import * as basicLightbox from "basiclightbox";
+import"basiclightbox/dist/basicLightbox.min.css";
+import "./sass/styles.scss";
+const openModalBtnRef = document.querySelector("button[data-open-modal]");
 
-console.log(basicLightbox);
+const modalTemplate = document.querySelector("#modal");
+
+const instance = basicLightbox.create(modalTemplate, {
+  onShow: (instance) => { // со стрелкой
+    const closeModalBtnRef = getCloseModalBtnRef(instance);
+    closeModalBtnRef.addEventListener("click", instance.close);
+  },
+  onClose(instance) { // без стрелки, без разницы в данном случае
+    const closeModalBtnRef = getCloseModalBtnRef(instance);
+    closeModalBtnRef.removeEventListener("click", instance.close);
+  },
+});
+
+openModalBtnRef.addEventListener("click", instance.show);
+
+function getCloseModalBtnRef(parent) {
+  return parent
+    .element()
+    .querySelector("button[data-close-modal]");
+}
