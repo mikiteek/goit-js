727d1c4e20cb5d4627a20c3500c2e8452f03078c
mikiteek <mikiteek@gmail.com>
2020-07-21 18:15:34 +0300

    mod-10 -> libraries: siema for slider

--- mod-10__libraries/src/index.js
+++ mod-10__libraries/src/index.js
@@ -1,3 +1,4 @@
 import "./js/modal";
+import "./js/carousel";
 import "./sass/styles.scss";
 
--- mod-10__libraries/src/js/carousel.js
+++ mod-10__libraries/src/js/carousel.js
@@ -1,1 +1,11 @@
 import Siema from "siema";
+
+const siema = new Siema({
+  duration: 500,
+});
+console.log(siema);
+const nextBtnRef = document.querySelector('.js-next');
+const prevBtnRef = document.querySelector('.js-prev');
+
+prevBtnRef.addEventListener('click', () => siema.prev());
+nextBtnRef.addEventListener('click', () => siema.next());
