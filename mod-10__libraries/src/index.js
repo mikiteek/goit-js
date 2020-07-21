f931a28647b92e2dbda2f5dceb68eb2dddf4ff60
mikiteek <mikiteek@gmail.com>
2020-07-21 17:18:48 +0300

    mod-10 -> libraries: separate index.js on different files, move basicLightbox modal to separate file

--- mod-10__libraries/src/index.js
+++ mod-10__libraries/src/index.js
@@ -1,25 +1,3 @@
-import * as basicLightbox from "basiclightbox";
-import"basiclightbox/dist/basicLightbox.min.css";
+import "./js/modal";
 import "./sass/styles.scss";
-const openModalBtnRef = document.querySelector("button[data-open-modal]");
 
-const modalTemplate = document.querySelector("#modal");
-
-const instance = basicLightbox.create(modalTemplate, {
-  onShow: (instance) => { // со стрелкой
-    const closeModalBtnRef = getCloseModalBtnRef(instance);
-    closeModalBtnRef.addEventListener("click", instance.close);
-  },
-  onClose(instance) { // без стрелки, без разницы в данном случае
-    const closeModalBtnRef = getCloseModalBtnRef(instance);
-    closeModalBtnRef.removeEventListener("click", instance.close);
-  },
-});
-
-openModalBtnRef.addEventListener("click", instance.show);
-
-function getCloseModalBtnRef(parent) {
-  return parent
-    .element()
-    .querySelector("button[data-close-modal]");
-}
