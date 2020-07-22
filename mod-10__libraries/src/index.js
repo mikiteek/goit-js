3c855d627dddb2f007b077a22b1d0954a2a58776
mikiteek <mikiteek@gmail.com>
2020-07-22 13:31:48 +0300

    mod-10 -> libraries: gumshoejs does not work

--- mod-10__libraries/src/index.js
+++ mod-10__libraries/src/index.js
@@ -2,3 +2,6 @@ import "./js/modal";
 import "./js/carousel";
 import "./sass/styles.scss";
 
+// why does not work - Repeta
+// import Gumshoe from "gumshoejs";
+// const gum = new Gumshoe("#page-nav a");
